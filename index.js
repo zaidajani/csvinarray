e8a74d6a928c3d5c1ae30ebbeb23f8c48c7fcf98
zaidajani <zaidajani2007@gmail.com>
2021-05-24 16:31:09 +0530

    problem-resolve

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const fs = require('fs');
 const path = require('path');
 
-function translate(csv, path) {
-  fs.readFileSync(path.join(__dirname + path));
+function translate(pathName) {
+  const csv = fs.readFileSync(pathName);
   let array = csv.toString().replace(/\n/g, '').split("\r");
-  return array
+  return array;
 }
 
+module.exports = translate;
